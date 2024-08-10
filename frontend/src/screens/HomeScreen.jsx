8f68428ec858f1e78d69ebe34b548b85d07eb6e3
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-10 19:42:52 +0500

    [ ] ensures that this effect runs only once, after the initial render

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,18 @@ import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data); 
+    };
+
+    fetchProducts();
+
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
