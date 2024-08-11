1a8864830d7bae24d127323adcc06e556e572cfd
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-11 23:46:02 +0500

    import connectDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express  from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/Products.js";
 const port = process.env.PORT || 5000;
 
