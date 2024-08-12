208969c08272aa1ddc88a7f467e39c2a597a1d88
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-12 16:41:13 +0500

    dummy data

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt.js';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Ahmad Zafar',
+        email: 'ahmadzafar@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false
+    },
+    {
+        name: 'Hassan Zafar',
+        email: 'hassanzafar@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false
+    }
+]
+
+export default users;
