7c321ad555b73ff645c60345a844156549255f04
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-15 18:45:29 +0500

    import loader in product screen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
 import { useGetProductDetailsQuery } from '../slices/productApiSlice';
 
 const ProductScreen = () => {
@@ -15,7 +16,7 @@ const ProductScreen = () => {
         Go Back
       </Link>
 
-    { isLoading ? (<h2>Loading...</h2>) : 
+    { isLoading ? (<Loader />) : 
     error ? (<div>{ error?.data.message || error.error }</div>) : 
     (
       <Row>
