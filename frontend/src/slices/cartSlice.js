852bad1c4943c9cd4d9f8ecbb7a47665a6e6768f
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-16 22:59:23 +0500

    calculating tax price

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -34,6 +34,8 @@ const cartSlice = createSlice({
             state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
 
             //Calculate tax price
+            state.taxPrice = addDecimals(Number ((0.15 * state.itemsPrice).toFixed(2)));
+
             //Calculate total price
         },
     },
