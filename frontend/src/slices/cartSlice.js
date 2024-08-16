65181d314d5210daa3ea092e39686c02e7fb4460
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-16 23:08:41 +0500

    comments...

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -30,10 +30,10 @@ const cartSlice = createSlice({
                 acc + item.price * item.qty, 0
             ));  
 
-            //Calculate shipping price
+            //Calculate shipping price (if order is over $100 then free, else $10 shipping)
             state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
 
-            //Calculate tax price
+            //Calculate tax price(15% tax)
             state.taxPrice = addDecimals(Number ((0.15 * state.itemsPrice).toFixed(2)));
 
             //Calculate total price
