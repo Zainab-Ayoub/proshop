75511084f79a3be6ee98658e45d814ade82eb8b4
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-18 16:01:52 +0500

    checkout button

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -72,6 +72,15 @@ const CartScreen = () => {
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
                 .toFixed(2)}
             </ListGroup.Item>
+            <ListGroup.Item>
+              <Button 
+                type='button'
+                className='btn-block' 
+                disabled={cartItems.length === 0}
+              >
+                Proceed To Checkout
+              </Button>  
+            </ListGroup.Item>
           </ListGroup>
         </Card>
       </Col>
