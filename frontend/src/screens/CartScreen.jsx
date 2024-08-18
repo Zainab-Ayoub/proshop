2eae9cd4e3cb148c5137fc447a66d0fcec7255d3
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-18 15:31:21 +0500

    count in stock in cart screen

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -31,9 +31,25 @@ const CartScreen = () => {
               <ListGroup.Item key={item._id}>
                  <Row>
                    <Col md={2}>
-                     <Image src={item.image} alt={item.name} fluid rounded />
+                      <Image src={item.image} alt={item.name} fluid rounded />
                    </Col>
-                   <Col></Col>
+                   <Col md={3}>
+                      <Link to={`/product/${item._id}`}>{item.name}</Link> 
+                   </Col>
+                   <Col md={2}>${item.price}</Col>
+                   <Col md={2}>
+                   <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) => {}} 
+                     >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                     </Form.Control>
+                   </Col>
                  </Row>
                </ListGroup.Item>
              ))}
