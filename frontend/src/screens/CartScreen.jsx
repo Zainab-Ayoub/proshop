356822e1dbfb99dd9f548f7816e9bee3e83e1415
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-18 15:14:12 +0500

    addToCartHandler

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -4,14 +4,43 @@ import { Row, Col, ListGroup, Image, Form, Button, Card }
 from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
+import { addToCart } from '../slices/cartSlice';
 
 const CartScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const addToCartHandler = async (product, qty) => {
+    dispatch(addToCart({...product, qty}));
+  };
+
   return (
-    <div>CartScreen</div>
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: '20px'}}>Shopping Cart</h1>
+        {/* {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                  </Col>
+                  <Col></Col>
+                </Row>
+              </ListGroup.Item>
+            ))} */}
+          </ListGroup>
+        )}
+      </Col>
+    </Row>
   )
 }
 
-export default CartScreen;
+export default CartScreen; 
