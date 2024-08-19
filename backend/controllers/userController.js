8c7b7e5828df0e49298af659b341063cff36c491
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-19 19:27:11 +0500

    authentication, register and logout user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,26 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import User from '../models/userModel.js';
 
-//@desc      Fetch all products
-//@route     GET api/products
+//@desc      Auth user & get token
+//@route     POST api/users/login
 //@access    Public
 
-const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+const authUser = asyncHandler(async (req, res) => {
+    res.send('auth user')
+});
+
+//@desc      Register user
+//@route     POST api/users
+//@access    Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    res.send('register user')
+});
+
+//@desc      Logout user / clear cookie
+//@route     POST api/users/logout
+//@access    Private
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.send('logout user')
 });
