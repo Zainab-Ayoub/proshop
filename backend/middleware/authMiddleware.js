361502f6562dc41f2568ded151945a8ddaa825a2
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-21 22:24:47 +0500

    admin middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,6 @@ import asyncHandler from './asyncHandler.js';
 import User from '../models/userModel.js';
 
 //  protect routes
-
 export const protect = asyncHandler(async (req, res, next) => {
     let token;
 
@@ -23,6 +22,18 @@ export const protect = asyncHandler(async (req, res, next) => {
       } 
     } else {
         res.status(401);
-        throw new Error('Not authorized, token failed');
+        throw new Error('Not authorized, no token');
     }
 })
+
+// admin middleware
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as admin');
+    }
+};
+
+export { protect, admin};
