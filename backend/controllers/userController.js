968fd975c0de9380bce29e26dfe0c3ad43f9fe28
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-21 23:07:01 +0500

    create user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,6 +63,11 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('User already exists');
     }
+    const user = await User.create({
+        name, 
+        email, 
+        password
+    });
 });
 
 //@desc      Logout user / clear cookie
