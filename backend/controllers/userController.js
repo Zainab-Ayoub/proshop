2745b8907a20486797589af16f415c6889ab3521
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-21 18:36:49 +0500

    enter 'jwt', token in res.cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,7 @@ const authUser = asyncHandler(async (req, res) => {
                 expiresIn: '30d'
             });
         // Set JWT as HTTP-Only Cookie
-        res.cookie({
+        res.cookie('jwt', token, {
           httpOnly: true,
           secure: process.env.NODE_ENV !== 'development',
           sameSite: 'strict',
