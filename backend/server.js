b1b0f18d0dce1c0a1d256a103100a23053f487a8
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-21 22:01:20 +0500

    import cookieParser and pass it to app.use

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
@@ -15,6 +16,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// cookie parser middleware
+
+app.use(cookieParser());
 
 app.use(cors());
 
