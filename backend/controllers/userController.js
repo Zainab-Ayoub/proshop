76d353921c4652fbab03344d4d20cbd96a6764a2
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-21 23:55:41 +0500

    request by id

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,7 +28,7 @@ const authUser = asyncHandler(async (req, res) => {
     if (user && (await user.matchPassword(password))) {
         generateToken(res, user._id);
 
-        res.json({
+        res.status(200).json({
         _id: user._id,
         name: user.name,
         email: user.email,
@@ -92,7 +92,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@access    Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(200).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+        });  
+      } else {
+        res.status(404);
+        throw new Error('Invalid user data');  
+      }
 });
 
 //@desc      Update user profile
