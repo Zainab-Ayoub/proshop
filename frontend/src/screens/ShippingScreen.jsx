99d1a6a5fd2e6acb8635a0ad894ca8a3a0c411be
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-25 01:39:41 +0500

    import useNavigate from react-router-dom

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from '../slices/cartSlice';
+import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../slices/cartSlice';
 
