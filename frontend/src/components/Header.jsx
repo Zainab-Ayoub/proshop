f90a2417be00732b09cd349260c56c53821e2c47
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-24 19:26:44 +0500

    logout handler

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from "react-router-bootstrap";
 import { useSelector } from "react-redux";
@@ -36,12 +36,25 @@ const Header = () => {
                                 }
                             </Nav.Link>
                         </LinkContainer>
-                        
-                        <LinkContainer to='/login'>
+                        { userInfo ? (
+                          <NavDropdown title={userInfo.name} id='username'>
+                            
+                            <LinkContainer to='profile'>
+                              <NavDropdown.Item>Profile</NavDropdown.Item>
+                            </LinkContainer>
+
+                            <NavDropdown.Item onClick={logoutHandler}>
+                                Logout
+                            </NavDropdown.Item> 
+
+                          </NavDropdown> 
+                        ) : (
+                            <LinkContainer to='/login'>
                             <Nav.Link href='/login'>  
                                <FaUser /> Sign In
                             </Nav.Link>
-                        </LinkContainer>    
+                        </LinkContainer>  
+                        )}  
 
                     </Nav>
                 </Navbar.Collapse>
