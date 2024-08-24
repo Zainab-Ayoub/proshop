600675d951e525e3027e3cf76cfb73b50c6188ec
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-25 01:26:42 +0500

    navigate to payment page

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from '../slices/cartSlice';
 import FormContainer from '../components/FormContainer';
+import { saveShippingAddress } from '../slices/cartSlice';
 
 const ShippingScreen = () => {
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [postalCode, setPostalCode] = useState('');
-  const [country, setCountry] = useState('');
+ 
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;  
+
+  const [address, setAddress] = useState(shippingAddress?.address || '');
+  const [city, setCity] = useState(shippingAddress?.city || '');
+  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
+  const [country, setCountry] = useState(shippingAddress?.country || '');
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShippingAddress({address, city, postalCode, country}));
+    navigate('/payment');
+  } 
 
   return (
-    <div>ShippingScreen</div>
+    <FormContainer>
+        ShippingScreen
+    </FormContainer>
   )
 }
 
