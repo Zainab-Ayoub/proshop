7cb2f6910404f15776a8fe659a0580aaac07e560
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-25 22:47:11 +0500

    add / before api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
 //@desc      Fetch all products
-//@route     GET api/products
+//@route     GET /api/products
 //@access    Public
 
 const getProducts = asyncHandler(async (req, res) => {
@@ -11,7 +11,7 @@ const getProducts = asyncHandler(async (req, res) => {
 });
 
 //@desc      Fetch all products
-//@route     GET api/products
+//@route     GET /api/products
 //@access    Public
 
 const getProductById = asyncHandler(async (req, res) => {
