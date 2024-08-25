e9fe09bf3c8ed8b83b603fcabb12f49289031a62
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-25 23:20:12 +0500

    import order routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import cors from 'cors';
 
 dotenv.config();
@@ -31,6 +32,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes); // Use productRoutes for API routes
 app.use('/api/users', userRoutes); // Use userRoutes for API routes
+app.use('/api/orders', orderRoutes); // Use orderRoutes for API routes
 
 app.use(notFound); // Middleware to handle 404 errors
 app.use(errorHandler); // Middleware to handle other errors
