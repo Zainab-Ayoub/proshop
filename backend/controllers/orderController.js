4b878bb2b9cb0f6912c75f8bb3b646ca4873e2e8
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-25 22:48:23 +0500

    Get logged in other users

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,9 +2,17 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import Order from '../models/orderModel.js';
 
 //@desc      Create new order 
-//@route     POST api/orders
+//@route     POST /api/orders
 //@access    Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
     res.send('add order items');
+});
+
+//@desc      Get logged in other users 
+//@route     GET /api/orders/myorders
+//@access    Private
+
+const getMyOrders = asyncHandler(async (req, res) => {
+    res.send('get my orders');
 });
