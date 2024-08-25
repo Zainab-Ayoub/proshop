42ec3ac6d8ea7a7ebdfd74d5c3a1b717e6d5b73b
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-25 22:45:41 +0500

    Create new order

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,10 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Order from '../models/orderModel.js';
+
+//@desc      Create new order 
+//@route     POST api/orders
+//@access    Private
+
+const addOrderItems = asyncHandler(async (req, res) => {
+    res.send('add order items');
+});
