f526b81b89ae88b4b3317549951f6375bd3384e6
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-27 17:27:27 +0500

    list group item for order info

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -14,6 +14,40 @@ const OrderScreen = () => {
         error,
     } = useGetOrderDetailsQuery(orderId);
 
+    return isLoading ? (
+        <Loader />
+    ) : error ? (
+        <Message variant='danger'/>
+    ) : (
+        <>
+          <h1>Order {order._id}</h1>
+          <Row>
+            <Col md={8}>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h2>Shipping</h2>
+                    <p><strong>Name: </strong> {order.user.name}</p>
+                    <p><strong>Email: </strong> {order.user.email}</p>
+                    <p><strong>Address: </strong>
+                    {order.shippingAddress.address}, {order.shippingAddress.city}{' '}
+                    {order.shippingAddress.postalCode}, {' '}
+                    {order.shippingAddress.country}
+                    </p>
+                    {order.isDelivered ? (
+                        <Message variant='success'>
+                            Delivered on {order.deliveredAt}
+                        </Message>
+                    ) : (
+                        <Message variant='danger'>Not Delivered</Message>
+                    )}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={4}></Col>
+          </Row>
+        </>
+    )
+
     return <div>OrderScreen</div>
 };
 export default OrderScreen;
