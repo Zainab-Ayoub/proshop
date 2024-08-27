b967465b56bded7a67c1fb7cf12104b039629fa9
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-27 17:43:22 +0500

    list group item for payment method

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -41,6 +41,17 @@ const OrderScreen = () => {
                         <Message variant='danger'>Not Delivered</Message>
                     )}
                 </ListGroup.Item>
+
+                <ListGroup.Item>
+                    <h2>Payment Method</h2>
+                    <p><strong>Method: </strong>{order.paymentMethod}</p>
+                    {order.isPaid ? (
+                        <Message variant='success'>Paid on {order.paidAt}</Message>
+                    ) : (
+                        <Message variant='danger'>Not Paid</Message>
+                    )}
+                </ListGroup.Item>
+
               </ListGroup>
             </Col>
             <Col md={4}></Col>
