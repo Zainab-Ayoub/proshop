8eb4ec82eea79708b0a049ec9977c64c950a71db
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-30 19:31:51 +0500

    table row

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -6,8 +6,31 @@ import Loader from '../../components/Loader';
 import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
 
 const OrderListScreen = () => {
+  const { data: orders, isLoading, error } = useGetOrdersQuery();
+
   return (
-    <div>OrderListScreen</div>
+    <>
+      <h1>Orders</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className='table-sm'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th></th>
+            </tr>
+          </thead>
+        </Table>
+      )}
+    </>
   )
 }
 
