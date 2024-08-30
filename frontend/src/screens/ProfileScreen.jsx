45b5b32cf47b9ae9fd635ded75d74fc6cb47740e
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-30 16:20:17 +0500

    form group for password

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -53,6 +53,16 @@ const ProfileScreen = () => {
               onChange={(e) => setEmail(e.target.value)}
             ></Form.Control>
           </Form.Group>
+
+          <Form.Group controlId="password" className="my-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
         </Form>
       </Col>
       <Col md={9}></Col>
