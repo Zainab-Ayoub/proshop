8caed7145b88fb354adf8408f5e122a764f12600
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-08-31 23:55:49 +0500

    product button

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import { useGetProductsQuery } from '../../slices/productsApiSlice';
 
 const ProductListScreen = () => {
   return (
-    <div>ProductListScreen</div>
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col> 
+        <Col className='text-end'>
+          <Button className='btn-sm m-3'>
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+    </>
   )
 }
 
-export default ProductListScreen;
+export default ProductListScreen;  
