b6dc53905c58a17d68fa6264aa7d55d65724a3da
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-02 00:20:19 +0500

    imports

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import FormContainer from '../../components/FormContainer';
+import { toast } from 'react-toastify';
+import { useUpdateProductMutation, 
+         useGetProductDetailsQuery } from '../../slices/productsApiSlice';
 
 const ProductEditScreen = () => {
   return (
