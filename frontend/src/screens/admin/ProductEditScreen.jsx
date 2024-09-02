257e0178bbec1e2bdb9b3bdcc351b78988a095b1
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-02 19:11:21 +0500

    uploadFileHandler function

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -67,6 +67,18 @@ const ProductEditScreen = () => {
     }
   }
 
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append('image', e.target.files[0]);
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  }
+
   return (
     <>
       <Link to='/admin/productlist' className='btn btn-light my-3'>
