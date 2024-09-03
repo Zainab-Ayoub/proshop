e26b5b249e8daaf1b49e2b51c99a637cdcf687ce
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-03 19:14:51 +0500

    download build

--- backend/server.js
+++ backend/server.js
@@ -42,21 +42,21 @@ app.get('/api/config/paypal', (req, res) => {
     res.json({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-const __dirname = path.resolve(); // set __dirname to current directory
+const __dirname = path.resolve(); // Set __dirname to the current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if(procees.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use('/uploads', express.static(path.join(__dirname, '/frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    //any route that is not api will be redirected to index.html
+    // Any route that is not API will be redirected to index.html
     app.get('*', (req, res) => 
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
 } else {
     app.get('/', (req, res) => {
         res.send('API is running...');
-    }); 
+    });
 }
 
 // Middleware to handle 404 errors
