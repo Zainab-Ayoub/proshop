5eae68532846349d835ed5b6abdfa9b9658d01f9
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-03 16:04:25 +0500

    import and use useParams

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,7 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit, FaTrash } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
 import {toast} from 'react-toastify';
@@ -9,8 +10,10 @@ import { useGetProductsQuery,
          useDeleteProductMutation
        } from '../../slices/productsApiSlice';
 
-const ProductListScreen = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+const ProductListScreen = () => { 
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error, refetch } = useGetProductsQuery({ pageNumber });
 
   const [createProduct, {isLoading: loadingCreate}] = useCreateProductMutation();
 
@@ -76,7 +79,7 @@ const ProductListScreen = () => {
             </thead>
 
             <tbody>
-              {products.map((product) => (
+              {data.products.map((product) => (
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
