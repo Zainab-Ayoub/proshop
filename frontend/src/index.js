20ade2286fae72e8d06a3303b3714900778297e4
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-03 16:06:06 +0500

    typo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -52,7 +52,8 @@ const router = createBrowserRouter(
 
       <Route path='' element={<AdminRoute />}>
         <Route path='/admin/orderList' element={<OrderListScreen />}/>
-        <Route path='/admin/productList' element={<ProductListScreen />}/>
+        <Route path='/admin/productlist' element={<ProductListScreen />}/>
+        <Route path='/admin/productlist/:pageNumber' element={<ProductListScreen />}/>
         <Route path='/admin/product/:id/edit' element={<ProductEditScreen />}/>
         <Route path='/admin/userlist' element={<UserListScreen />}/>
         <Route path='/admin/user/:id/edit' element={<UserEditScreen />}/>
