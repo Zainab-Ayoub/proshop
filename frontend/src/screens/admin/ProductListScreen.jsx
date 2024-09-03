34514eea0a70434e78344f1c0580b81b85afefdb
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-03 16:09:13 +0500

    import Paginate

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -4,6 +4,7 @@ import { FaEdit, FaTrash } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import {toast} from 'react-toastify';
 import { useGetProductsQuery, 
          useCreateProductMutation,
@@ -100,6 +101,7 @@ const ProductListScreen = () => {
               ))}
             </tbody>
           </Table>
+          <Paginate pages={data.pages} page={data.page} isAdmin={true} />
         </>
       )}
     </>
